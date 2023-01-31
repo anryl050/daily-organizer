54c28a5d78dbd5d671ab99eb250939ceb330a239
anryl050 <118693401+anryl050@users.noreply.github.com>
2023-01-30 16:49:26 -0800

    revised the application to make it a daily activitity scheduler; added time to the display in the header, next to the day and date format

--- assets/script.js
+++ assets/script.js
@@ -24,7 +24,7 @@ $(function () {
 
   // Add a for loop to increment the hours throughout the day. past, present, and future classes are added to the time block based on the curremt hour of the work day.  
   var currentTime = parseInt(hour);
-    for (var i = 9; i <= 24; i++) {
+    for (var i = 5; i <= 24; i++) {
       if ( currentTime < i ) {
         $("#hour-"+i).addClass('future');
       } else if ( currentTime === i ) {
@@ -33,27 +33,15 @@ $(function () {
         $("#hour-"+i).addClass('past');
       };
     }
-
-  // var currentTime = dayjs().format("H");
-  // $(".timer-block").each(function(){
-  //   var timeBlock = parseInt($(this).attr('id').split("hour")[2]);
-
-  //   if (timeBlock < currentTime) {
-  //     $(this).addClass('future');
-  //   }
-  //   else if (timeBlock === currentTime) {
-  //     $(this).addClass('present');
-  //   } 
-  //   else {
-  //     $(this).addClass('past');
-  //   }
-  // });
  
   // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. 
 
   // Retrieve the stored values from local storage
   // $("#hour-9 .description") -> refers to the id(#hour-9 and class(.description). Due to referencing 2 different elements, the space is required between the 2.
   // Returns the current value (string) associated with the given key(i.e. hour-9).
+  $("#hour-6 .description").val(localStorage.getItem("hour-6"));
+  $("#hour-7 .description").val(localStorage.getItem("hour-7"));
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -63,12 +51,17 @@ $(function () {
   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-          
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+  $("#hour-19 .description").val(localStorage.getItem("hour-19"));
+  $("#hour-20 .description").val(localStorage.getItem("hour-20"));
+  $("#hour-21 .description").val(localStorage.getItem("hour-21"));
+  $("#hour-22 .description").val(localStorage.getItem("hour-22"));
+  $("#hour-23 .description").val(localStorage.getItem("hour-23"));        
   
   // TODO: Add code to display the current date in the header of the page.
-  
+
   // creates the var with desired Day.js format (i.e. Sunday, January 29 2023)
-  var today = dayjs().format("[Today is] dddd, MMMM D YYYY");  
+  var today = dayjs().format("[Today is] dddd, MMMM D YYYY HH:mm");  
   // check point
   console.log(today);
   // displays the curent date
